c0f2dc3ad2de6f7ee14d8a621c3920fdbc40fbe4
MaciejBartczak <maciej.bartczak7@gmail.com>
2016-10-17 22:18:19 +0200

    Add project files.

---
+++ ReverseString/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ReverseString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReverseButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Cleaner = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ReverseButton
+            // 
+            this.ReverseButton.Location = new System.Drawing.Point(12, 12);
+            this.ReverseButton.Name = "ReverseButton";
+            this.ReverseButton.Size = new System.Drawing.Size(75, 23);
+            this.ReverseButton.TabIndex = 0;
+            this.ReverseButton.Text = "Reverse";
+            this.ReverseButton.UseVisualStyleBackColor = true;
+            this.ReverseButton.Click += new System.EventHandler(this.ReverseButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            // 
+            // Cleaner
+            // 
+            this.Cleaner.Location = new System.Drawing.Point(94, 12);
+            this.Cleaner.Name = "Cleaner";
+            this.Cleaner.Size = new System.Drawing.Size(75, 23);
+            this.Cleaner.TabIndex = 3;
+            this.Cleaner.Text = "Clean textbox";
+            this.Cleaner.UseVisualStyleBackColor = true;
+            this.Cleaner.Click += new System.EventHandler(this.Cleaner_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.Cleaner);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ReverseButton);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Reverse string";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReverseButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Cleaner;
+    }
+}
+
---
+++ ReverseString/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReverseString
+{
+    public partial class Form1 : Form
+    {
+        private string text;
+        private string reversedText;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+
+        private void ReverseButton_Click(object sender, EventArgs e)
+        {
+            if (text == textBox1.Text)
+                return;
+            label1.Location = new Point(textBox1.Location.X, textBox1.Height + textBox1.Location.Y + 20);
+            reversedText = string.Empty;
+            for (int i = textBox1.TextLength - 1; i > -1; i--)
+            {
+                reversedText += textBox1.Text.Substring(i, 1);
+            }
+            label1.Text = reversedText;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.TextLength > 15)
+            textBox1.Width = textBox1.TextLength * 6;
+        }
+
+        private void Cleaner_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            label1.Text = string.Empty;
+        }
+    }
+}
